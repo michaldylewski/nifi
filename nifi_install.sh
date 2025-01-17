e850336cde22334794d9f59e6d590330b24ae92d
michaldylewski <mdylewski.s@gmail.com>
2025-01-17 17:16:18 +0100

    Update nifi_install.sh

--- nifi_install.sh
+++ nifi_install.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
-IP=$(curl http://checkip.amazonaws.com)
+AWS_NIFI_WEB_HTTP_HOST=$(curl http://checkip.amazonaws.com)
 
-echo "$IP"
+echo "$AWS_NIFI_WEB_HTTP_HOST"
 
-#sed "s/IP/$IP/" nsupdate.txt | nsupdate
 # Instalacja repo
 
 sudo apt install -y apt-transport-https ca-certificates curl software-properties-common unzip
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" -y
 sudo apt update
 apt-cache policy docker-ce
 
@@ -25,4 +24,19 @@ sudo chmod +x /usr/local/bin/docker-compose
 
 sudo usermod -aG docker ${USER}
 
-exec bash
+source ~/.bashrc
+source ~/.profile
+
+#sudo systemctl restart docker.service
+
+# Instalacja NiFi
+
+cd ~ && wget https://raw.githubusercontent.com/michaldylewski/nifi/refs/heads/main/docker-compose.yml
+
+cd ~ && (sed -i -e "s/AWS_NIFI_WEB_HTTP_HOST/$AWS_NIFI_WEB_HTTP_HOST/g" docker-compose.yml)
+
+# Uruchomienie docker-compose w tle
+cd ~ && sudo docker-compose up -d
+
+# Możesz dodać inne komendy po uruchomieniu docker-compose
+echo "Docker Compose został uruchomiony w tle."
